a43540b44ec1d4660ee4df103dcf7b1e1bb31dce
ben200045 <ben@netrix.com.au>
2022-08-10 14:36:45 +0800

    Frame Operational

---
+++ src/containers/Frame_Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from '../css/Frame_Container.module.css';
+
+export default function(){
+    return (
+        <div className={styles.frame}>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import styles from './css/index.css';
 import reportWebVitals from './reportWebVitals';
+import Frame_Container from './containers/Frame_Container';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+  <BrowserRouter>
+  <div className={styles.frame_setup}>  
+    <Routes>
+      <Route path="/" element={<Frame_Container />} />
+      <Route path="Favourites" element={<Frame_Container />} />
+    </Routes>
+    </div>
+  </BrowserRouter>
+  </div>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
