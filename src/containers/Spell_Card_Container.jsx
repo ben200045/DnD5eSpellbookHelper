2f4b1d204117f41adbb986ee358f157879c91f3e
ben200045 <ben@netrix.com.au>
2022-08-11 20:16:43 +0800

    refactor: localised Containers to single CSS

--- src/containers/Spell_Card_Container.jsx
+++ src/containers/Spell_Card_Container.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from '../css/Spell_CardList.module.css';
+import styles from '../css/Frame_Container.module.css';
 
 export default class Spell_CardList extends React.Component{
     render () {
         return(
             <div className={styles.spell_cardlist}>
-
+                
             </div>
         );
     }
