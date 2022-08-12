19caed282dab59a232c9026abddd8b8c8cb4ef10
ben200045 <ben@netrix.com.au>
2022-08-13 00:08:25 +0800

    feat: implemented API call

--- src/containers/Spell_Card_Container.jsx
+++ src/containers/Spell_Card_Container.jsx
@@ -3,10 +3,37 @@ import Spell_Card from './Spell_Card';
 import styles from '../css/Frame_Container.module.css';
 
 export default class Spell_CardList extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            spell_cards: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount(){
+
+        fetch('https://www.dnd5eapi.co/api/spells')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    spell_cards: json.cards,
+                });
+            }).catch(err => {
+            console.log(err);
+        }
+        );
+    }
+
     render () {
         return(
             <div className={styles.spell_cardlist}>
-
+                {this.state.spell_cards.map(spell_card => ( 
+                <Spell_Card 
+                    
+                />))} );
             </div>
         );
     }
