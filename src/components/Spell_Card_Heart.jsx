5c74d5bacea84467360dc2e795f0c605d6991402
ben200045 <ben@netrix.com.au>
2022-08-12 11:12:14 +0800

    feat: implemented Spell_Card components

---
+++ src/components/Spell_Card_Heart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../css/Spell_Card.module.css';
+
+export default class Spell_Card_Heart extends React.Component {
+    render () {
+        return (
+            <>
+                <div className={styles.spell_card_heart}></div>
+            </>
+        );
+    }
+}
--- src/containers/Spell_Card_Container.jsx
+++ src/containers/Spell_Card_Container.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Spell_Card from './Spell_Card';
 import styles from '../css/Frame_Container.module.css';
 
 export default class Spell_CardList extends React.Component{
     render () {
         return(
             <div className={styles.spell_cardlist}>
-                
+
             </div>
         );
     }
