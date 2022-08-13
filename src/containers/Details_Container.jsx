a6be617d84db4ce40ffd2f58304fba081fde53a4
ben200045 <ben@netrix.com.au>
2022-08-13 14:49:29 +0800

    feat: details UI added

--- src/containers/Details_Container.jsx
+++ src/containers/Details_Container.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import styles from '../css/Frame_Container.module.css';
+import styles from '../css/Details_Container.module.css';
 
 export default class Details_Container extends React.Component {
     render() {
         return (
             <div className={styles.details_container}>
-                
+                <div className={styles.details_borderbox}>
+                    <div className={styles.details_header}>
+                    </div>
+                    <div className={styles.damage_header}>
+                    </div>
+                </div>
+                <div className={styles.details_description}>
+                    <b>Description:</b><br />
+                </div>
             </div>
         );
     }
