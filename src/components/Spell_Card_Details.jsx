9ec32b60282f4ca4e8127849e2a26e01c6f87f45
ben200045 <ben@netrix.com.au>
2022-08-13 14:01:16 +0800

    fix: rolled back, needless API calls

--- src/components/Spell_Card_Details.jsx
+++ src/components/Spell_Card_Details.jsx
@@ -10,44 +10,50 @@ export default class Spell_Card_Details extends React.Component {
         }
     }
 
-    componentDidMount(){
-        const apiCall = 'https://www.dnd5eapi.co';
-        fetch(apiCall+this.props.url)
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    spell_details: json,
+    // componentDidMount(){
+    //     const apiCall = 'https://www.dnd5eapi.co';
+    //     fetch(apiCall+this.props.url)
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             this.setState({
+    //                 isLoaded: true,
+    //                 spell_details: json,
                     
-                });
-                console.log(this.state.spell_details);
-            }).catch(err => {
-            console.log(err);
-        }
-        );
-    }
+    //             });
+    //             console.log(this.state.spell_details);
+    //         }).catch(err => {
+    //         console.log(err);
+    //     }
+    //     );
+    // }
+
+ 
 
     render () {
 
-        const {spell_details, isLoaded} = this.state;
-        if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            console.log(spell_details.results);
+        function handleClick(){
+            console.log('clicked ');
+
+        }
+
+        // const {spell_details, isLoaded} = this.state;
+        // if (!isLoaded) {
+        //     return <div>Loading...</div>;
+        // } else {
+        //     console.log(spell_details.results);
 
         return (
             
-                <div className={styles.spell_card_details}>
+                <div className={styles.spell_card_details} onClick={handleClick}>
                     
                     {this.props.name}<br/>
-                    {spell_details.school.name}<br/>
+                    {/* {spell_details.school.name}<br/>
                     {spell_details.classes.map(class_ => 
                         {
                             return class_.name
-                        }).join(', ')}<br/>
+                        }).join(', ')}<br/> */}
                 </div>
             
         );
     }
-}
 }
