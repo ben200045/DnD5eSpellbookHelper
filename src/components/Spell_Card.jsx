d619c762c96a3aa9cd1595b74481f4e57a29f40f
ben200045 <ben@netrix.com.au>
2022-08-15 13:08:19 +0800

    fix: git branching

--- src/components/Spell_Card.jsx
+++ src/components/Spell_Card.jsx
@@ -1,31 +1,51 @@
-import React from "react";
-import Spell_Card_Details from "./Spell_Card_Details";
-import Spell_Card_Heart from "./Spell_Card_Heart";
-import styles from "../css/Spell_Card.module.css";
+import React from 'react';
+import Spell_Card_Details from './Spell_Card_Details';
+import Spell_Card_Heart from './Spell_Card_Heart';
+import styles from '../css/Spell_Card.module.css';
 
 export default class Spell_Card extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoaded: false,
-		};
-	}
 
-	render() {
-		return (
-			<div className={styles.spell_card}>
-				<Spell_Card_Details
-					name={this.props.name}
-					url={this.props.url}
-					getSpellURL={this.props.getSpellDetails[0]}
-				/>
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoaded: false,
+            // spellURL: '',
+        }
+    }
 
-				<Spell_Card_Heart
-					getFavouritedSpell={this.props.getSpellDetails[1]}
-					name={this.props.name}
-					url={this.props.url}
-				/>
-			</div>
-		);
-	}
+    render() {
+
+        // const getSpellURL = (spellURL) => {
+        //     this.setState({spellURL});
+        //   }
+//TODO: this code looks wonky, fix it
+    return (
+        <div className={styles.spell_card}>
+            {this.props.getSpellDetails ? 
+                <Spell_Card_Details
+                name={this.props.name}
+                url={this.props.url} 
+                getSpellURL={this.props.getSpellDetails[0]}
+                /> : 
+                <Spell_Card_Details 
+                    // spellURL={this.props.spellURL}
+                    // getSpellDetails={this.props.getSpellDetails}
+                />
+            }
+            {this.props.getSpellDetails ?
+            <Spell_Card_Heart 
+                getFavouritedSpell={this.props.getSpellDetails[1]}
+                name={this.props.name}
+                url={this.props.url}
+            /> :
+            <Spell_Card_Heart 
+
+            />
+            }
+
+        </div>
+    );
+
+    
+    }
 }
