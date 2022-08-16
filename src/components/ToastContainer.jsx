cf112f4f9dd7974fe44bd8f021fa125427fafaba
ben200045 <ben@netrix.com.au>
2022-08-17 00:13:47 +0800

    feat: added popup toast messages

---
+++ src/components/ToastContainer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+export default ToastContainer = () => {
+<ToastContainer
+position="top-left"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
+}
--- src/containers/Frame_Container.jsx
+++ src/containers/Frame_Container.jsx
@@ -3,7 +3,8 @@ import Spell_CardList from "./Spell_Card_Container";
 import Navbar_Container from "./Navbar_Container";
 import Details_Container from "./Details_Container";
 import styles from "../css/Frame_Container.module.css";
-
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default class Frame_Container extends React.Component {
 	constructor(props) {
 		super(props);
@@ -16,7 +17,7 @@ export default class Frame_Container extends React.Component {
 
 	render() {
 
-		
+
 		const getTabLocation = (tabLocation) => {
 			this.setState({ tabLocation });
 		};
@@ -28,9 +29,30 @@ export default class Frame_Container extends React.Component {
 		//TODO: this is a MESS
 		const getFavouritedSpells = (favouritedSpell) => {
 			const appendedList = this.state.favouritedSpells.concat(favouritedSpell);
-			this.state.favouritedSpells.some((spell) => spell.name === favouritedSpell.name) && this.state.favouritesTab === false
-				? this.setState({favouritedSpells: this.state.favouritedSpells.filter((spell) => spell.name !== favouritedSpell.name),})
-				: this.setState({ favouritedSpells: appendedList })
+			if(this.state.favouritedSpells.some((spell) => spell.name === favouritedSpell.name) && this.state.favouritesTab === false) {
+				 this.setState({favouritedSpells: this.state.favouritedSpells.filter((spell) => spell.name !== favouritedSpell.name),}) 
+				 toast.error(favouritedSpell.name +' Favourite Removed', {
+					position: "top-left",
+					autoClose: 2000,
+					hideProgressBar: true,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: false,
+					progress: undefined,
+					});
+				
+				} else {
+				 this.setState({ favouritedSpells: appendedList })
+				toast.success(favouritedSpell.name +' Favourite Added', {
+					position: "top-left",
+					autoClose: 2000,
+					hideProgressBar: true,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: false,
+					progress: undefined,
+					});
+				 }
 		};
 
 
@@ -38,6 +60,7 @@ export default class Frame_Container extends React.Component {
 		return (
 			<div className={styles.frame}>
 				<Navbar_Container getTabLocation={getTabLocation} />
+				<ToastContainer />
 				<div className={styles.maincontent} key={this.props.favourites}>
 					{this.props.favourites ? (
 						<Spell_CardList
